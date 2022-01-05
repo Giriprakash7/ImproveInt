5f357269c0cb9805d822cafae8338af5cb7cb9fb
Giriprakash7 <giriprakashkarmala1997@gmail.com>
2022-01-05 19:16:26 +0530

    full project

---
+++ src/main/java/Preparation/AnimalsClass.java
@@ -0,0 +1,5 @@
+package Preparation;
+
+public class AnimalsClass {
+
+}
---
+++ src/test/java/com/example/ImproveInt/ImproveIntApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ImproveInt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImproveIntApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
