8d3e2667404c101bb27fb5d2a260b99906844543
Giriprakash7 <giriprakashkarmala1997@gmail.com>
2022-01-05 14:35:16 +0530

    First push

---
+++ src/main/java/com/example/ImproveInt/ImproveIntApplication.java
@@ -0,0 +1,13 @@
+package com.example.ImproveInt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImproveIntApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImproveIntApplication.class, args);
+	}
+
+}
